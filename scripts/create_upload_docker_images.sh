e67f1f2337aeae647c5e57bf5518f64278688f17
franrobles8 <franrobles8@gmail.com>
2021-03-20 15:02:22 +0100

    Script to generate server image and push it to registry

---
+++ scripts/create_upload_docker_images.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+ENDCOLOR="\033[0m"
+
+DOCKER_HUB_USER=franrobles8
+
+################
+# Server service
+################
+
+SERVER_SERVICE=eoloplants_server
+SERVER_SERVICE_REPO="${DOCKER_HUB_USER}/${SERVER_SERVICE}"
+
+echo -e "${GREEN}Building service image: [server]${ENDCOLOR}"
+docker build -t $SERVER_SERVICE_REPO ./server
+
+echo -e "${GREEN}Pushing image: [server]${ENDCOLOR}"
+docker push $SERVER_SERVICE_REPO
+
+if [-z $?]; then
+    echo -e "${RED}Check that you have logged in to a docker repo. Try \"docker login\"${ENDCOLOR}"
+    exit 1
+fi
+
+echo -e "${BLUE}Images have been generated and pushed to the docker registry${ENDCOLOR}"
